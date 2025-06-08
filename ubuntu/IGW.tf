0099cc696d35000b61b065cb7e8610df453f137b
asare905 <asare905@gmail.com>
2025-06-08 18:11:37 -0500

    Delete ubuntu directory

--- ubuntu/IGW.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "container_igw" {
-    vpc_id = aws_vpc.container_vpc.id
-    tags = {
-      Name = "container_igw"
-    }
-}
--- ubuntu/subnets.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id     = aws_vpc.container_vpc.id
-  cidr_block = var.publi_web_1_cidr
-  availability_zone = var.zone-1
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public_subnet_1"
-  }
-}
